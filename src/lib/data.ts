7602bbf90f14800fb1b9fb88b6fa1070ee712937
Claude Bouchard <dualB@users.noreply.github.com>
2026-03-31 17:12:16 -0400

    test async

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,6 +1,6 @@
 import * as fs from "fs";
-
-
+import { AnyObject, Schema } from "./schema";
+import { TableModel } from "./modele";
 
 /**
  * Interface pour les tables
@@ -9,72 +9,91 @@ export interface ITable<T = any> {
   toObject(): T[];
 }
 
+export interface Database{
+  createTable<T extends AnyObject>(
+    name: string,
+    schema: Schema<T>
+  ): TableModel<T>
+}
+
 /**
  * Dictionnaire de tables
  */
-export const allTables: Record<string, ITable> = {};
 
 /**
  * Données brutes chargées depuis le disque
  */
-export const fullDatas: Record<string, any[]> = {};
 
-/**
- * Sérialisation complète de la DB
- */
-function formatForFile(): string {
-  const dbContent: Record<string, unknown> = {};
 
-  Object.keys(allTables).forEach((key) => {
-    dbContent[key] = allTables[key].toObject();
-  });
-
-  return JSON.stringify(dbContent, null, 2);
-}
 
 /**
  * Fonctions redéfinies dynamiquement après connect()
  */
-export let saveDb: () => Promise<void> = async () => {
-  console.warn(
-    "Aucune sauvegarde effectuée. La base de donnée ne sera qu'en mémoire."
-  );
-};
 
-let loadDb: () => void = () => {
-  console.warn("La base de donnée ne sera qu'en mémoire.");
-};
 
 /**
  * Connexion à une DB fichier
  */
-export function connect(uri: string, startFromScratch = false): void {
+export async function connect(uri: string, startFromScratch = false): Promise<Database> {
+
+  const fullDatas: Record<string, any[]> = {};
+  const allTables: Record<string, ITable> = {};
+  let saveDb: () => Promise<void> = async () => {
+    console.warn(
+      "Aucune sauvegarde effectuée. La base de donnée ne sera qu'en mémoire."
+    );
+  };
+
+  let loadDb: () => void = () => {
+    console.warn("La base de donnée ne sera qu'en mémoire.");
+  };
+
   loadDb = () => {
     try {
-      if (!fs.existsSync(uri)) return;
+      if (!fs.existsSync(uri)) {
+        console.warn(`Le fichier ${uri} est inexistant...`)
+        return;
+      }
 
       const data = fs.readFileSync(uri, { encoding: "utf8" });
-      const real: Record<string, {id:string}[]> = JSON.parse(data);
+      const real: Record<string, { id: string }[]> = JSON.parse(data);
 
       Object.keys(real).forEach((key) => {
         fullDatas[key] = real[key];
       });
+      console.log(`Le fichier ${uri} a été chargé.`)
+      saveDb = async () => {
+        try {
+          fs.writeFileSync(uri, formatForFile());
+        } catch (err) {
+          console.error(err);
+        }
+      };
+
     } catch (err) {
       console.error(err);
     }
   };
 
-  saveDb = async () => {
-    try {
-      fs.writeFileSync(uri, formatForFile());
-    } catch (err) {
-      console.error(err);
-    }
-  };
+  /**
+ * Sérialisation complète de la DB
+ */
+  function formatForFile(): string {
+    const dbContent: Record<string, unknown> = {};
+
+    Object.keys(allTables).forEach((key) => {
+      dbContent[key] = allTables[key].toObject();
+    });
+
+    return JSON.stringify(dbContent, null, 2);
+  }
+
+
 
   if (!startFromScratch) {
     loadDb();
   }
+  return Promise.resolve()
 }
 
 export default { connect };
